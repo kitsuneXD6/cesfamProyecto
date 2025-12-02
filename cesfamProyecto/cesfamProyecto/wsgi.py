08423e76e630ffa0594b6094fd2f1ee0126017b5
kitsune <tu@ejemplo.com>
2025-12-02 11:21:51 -0300

    Corregir ruta de settings en wsgi.py para Render

--- cesfamProyecto/cesfamProyecto/wsgi.py
+++ cesfamProyecto/cesfamProyecto/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cesfamProyecto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cesfamProyecto.cesfamProyecto.settings')
 
 application = get_wsgi_application()
